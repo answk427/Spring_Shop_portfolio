73f3c0af046695a4bb1d95bc48e644e19f6297c5
wlstjd50 <answk426@naver.com>
2026-01-16 23:36:24 +0900

    PersistenceContext는 필드 주입. final 변수 사용 불가

--- src/main/java/work/trade/user/repository/UserRepositoryImpl.java
+++ src/main/java/work/trade/user/repository/UserRepositoryImpl.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 public class UserRepositoryImpl implements UserRepository{
 
     @PersistenceContext
-    private final EntityManager em;
+    private EntityManager em;
 
     private final UserMapper userMapper;
 
