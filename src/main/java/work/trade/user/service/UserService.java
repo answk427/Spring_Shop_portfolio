e4a48c804dede419af28ea14da6d12a260ccc9a3
wlstjd50 <answk426@naver.com>
2026-01-18 00:21:32 +0900

    UserService CRUD 구현

--- src/main/java/work/trade/user/service/UserService.java
+++ src/main/java/work/trade/user/service/UserService.java
@@ -1,10 +1,19 @@
 package work.trade.user.service;
 
+import work.trade.product.dto.request.ProductCreateRequestDto;
+import work.trade.product.dto.request.ProductUpdateDto;
+import work.trade.product.dto.response.ProductDto;
 import work.trade.user.domain.User;
+import work.trade.user.dto.request.UserUpdateDto;
+import work.trade.user.dto.response.UserDto;
+
+import java.util.Optional;
 
 public interface UserService {
 
-    User getUser(Long id);
-
-    void createUser(User user);
+    //CRUD
+    UserDto createUser(User user);
+    Optional<UserDto> findUser(Long id);
+    UserDto updateUser(Long id, UserUpdateDto dto);
+    void deleteById(Long id);
 }
--- src/main/java/work/trade/user/service/UserServiceImpl.java
+++ src/main/java/work/trade/user/service/UserServiceImpl.java
@@ -1,29 +1,51 @@
 package work.trade.user.service;
 
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import work.trade.user.domain.User;
+import work.trade.user.dto.request.UserUpdateDto;
+import work.trade.user.dto.response.UserDto;
+import work.trade.user.mapper.UserMapper;
 import work.trade.user.repository.UserRepository;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+//Util----------------------------//
+    private String createPasswordHash(String password) {
+        //todo : Hash 생성 후 반환
+        return password;
+    }
+//----------------------------//
 
     @Override
-    @Transactional
-    public User getUser(Long id) {
-        Optional<User> findUser = userRepository.findById(id);
-        return findUser.get();
+    public UserDto createUser(User user) {
+        return userMapper.toDto(userRepository.save(user));
     }
 
     @Override
-    @Transactional
-    public void createUser(User user) {
-        userRepository.save(user);
+    public Optional<UserDto> findUser(Long id) {
+        return userRepository.findById(id).map(userMapper::toDto);
+    }
+
+    @Override
+    public UserDto updateUser(Long id, UserUpdateDto dto) {
+        User updatedUser = userRepository.update(id, dto);
+        updatedUser.setPasswordHash(createPasswordHash(dto.getPassword()));
+        return userMapper.toDto(updatedUser);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.delete(id);
     }
 }
