9e5b8c4d78fb93cfb5f64210f3ca42be5ec7af29
wlstjd50 <answk426@naver.com>
2026-03-30 16:44:04 +0900

    RestController 추가

---
+++ src/main/java/work/trade/common/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package work.trade.common.exception.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import work.trade.common.exception.BusinessException;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<String> handle(Exception e) {
+//        return ResponseEntity.internalServerError().body("서버 오류가 발생했습니다.");
+//    }
+
+    //BusinessException
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<String> handle(BusinessException e) {
+        return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+    }
+
+    //@Valid 검증 실패
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handle(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.badRequest().body(message);
+    }
+
+}
