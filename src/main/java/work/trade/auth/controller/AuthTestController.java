d01611fdbf5167a8716c4cd96f320c475e0337b4
wlstjd50 <answk426@naver.com>
2026-03-30 17:15:41 +0900

    인증 - 테스트 controller 추가

---
+++ src/main/java/work/trade/auth/controller/AuthTestController.java
@@ -0,0 +1,49 @@
+package work.trade.auth.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import work.trade.auth.dto.request.LoginRequestDto;
+import work.trade.auth.dto.response.LoginResponseDto;
+import work.trade.auth.jwt.JwtTokenUtil;
+import work.trade.auth.role.Role;
+
+import java.util.List;
+
+@RestController
+@RequestMapping()
+@RequiredArgsConstructor
+public class AuthTestController {
+
+    private final JwtTokenUtil jwtTokenUtil;
+
+    //permitAll 적용확인1
+    @GetMapping("/auth/test")
+    public String permitAllTest() {
+        return "ok";
+    }
+
+    //permitAll 적용확인1
+    @GetMapping("/notPermit")
+    public String notPermit() {
+        return "ok";
+    }
+
+    //토큰이 반환되는지 확인
+    @PostMapping("/auth/test/login")
+    public LoginResponseDto login(@RequestBody @Valid LoginRequestDto requestDto) {
+        return new LoginResponseDto(jwtTokenUtil.createToken(requestDto.getId(), List.of(Role.USER)));
+    }
+
+    //토큰을 실어서 보냈을 시 통과되는지 확인
+    @GetMapping("/requestWithToken")
+    public String authTest() {
+        return "ok";
+    }
+
+    //관리자 권한 확인
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
