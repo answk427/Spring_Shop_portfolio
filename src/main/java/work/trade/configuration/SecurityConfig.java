dfd2adda7cc4a91fe90bd0fdc8bb199262eef0d0
wlstjd50 <answk426@naver.com>
2026-03-30 17:16:41 +0900

    인증 - SecurityConfig 추가

---
+++ src/main/java/work/trade/configuration/SecurityConfig.java
@@ -0,0 +1,56 @@
+package work.trade.configuration;
+
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import work.trade.auth.filter.JwtAuthenticationFilter;
+import work.trade.auth.jwt.JwtTokenUtil;
+import work.trade.auth.role.Role;
+import work.trade.auth.security.JwtAccessDeniedHandler;
+import work.trade.auth.security.JwtAuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtTokenUtil);
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // 페이지별 출입 권한 설정
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
+                        .anyRequest().authenticated()
+                )
+                //필터 추가
+                .addFilterBefore(
+                        jwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+                        .accessDeniedHandler(new JwtAccessDeniedHandler())
+                );
+
+        return http.build();
+    }
+}
