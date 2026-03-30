629b2483dd9f1ef57e54ffa59f593c859f06f6c1
wlstjd50 <answk426@naver.com>
2026-03-30 17:15:32 +0900

    인증 - 로그인 controller 추가

---
+++ src/main/java/work/trade/auth/controller/AuthController.java
@@ -0,0 +1,25 @@
+package work.trade.auth.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import work.trade.auth.dto.request.LoginRequestDto;
+import work.trade.auth.dto.response.LoginResponseDto;
+import work.trade.auth.jwt.JwtTokenUtil;
+import work.trade.auth.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public LoginResponseDto login(@RequestBody @Valid LoginRequestDto requestDto) {
+        return authService.login(requestDto);
+    }
+
+}
+ 
