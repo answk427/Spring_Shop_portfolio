58ae4aa9ea0d702760def8911139f2a24d50677f
wlstjd50 <answk426@naver.com>
2026-04-01 14:49:12 +0900

    productController 추가 SecurityConfig 상품조회 허용

--- src/main/java/work/trade/configuration/SecurityConfig.java
+++ src/main/java/work/trade/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package work.trade.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -39,6 +40,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 //필터 추가
---
+++ src/main/java/work/trade/product/controller/productController.java
@@ -0,0 +1,57 @@
+package work.trade.product.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import work.trade.product.dto.request.ProductCreateRequestDto;
+import work.trade.product.dto.request.ProductUpdateDto;
+import work.trade.product.dto.response.ProductDto;
+import work.trade.product.dto.response.ProductSummaryDto;
+import work.trade.product.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class productController {
+    private final ProductService productService;
+
+    //상품 등록
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody @Valid ProductCreateRequestDto dto,
+            Authentication authentication) {
+        Long sellerId = Long.parseLong(authentication.getName());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(productService.createProduct(dto, sellerId));
+    }
+
+    //상품 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.findProduct(id));
+    }
+
+    //상품 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(
+            @PathVariable Long id,
+            @RequestBody @Valid ProductUpdateDto dto,
+            Authentication authentication) {
+        Long sellerId = Long.parseLong(authentication.getName());
+        return ResponseEntity.ok(productService.updateProduct(dto, sellerId));
+    }
+
+    //상품 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(
+            @PathVariable Long id,
+            Authentication authentication) {
+        Long sellerId = Long.parseLong(authentication.getName());
+        productService.deleteById(id, sellerId);
+        return ResponseEntity.noContent().build();
+    }
+}
