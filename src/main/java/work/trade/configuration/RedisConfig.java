78165354f452dae72ccfb95c94d35fccdb3cebaa
wlstjd50 <answk426@naver.com>
2026-04-10 11:50:26 +0900

    RedisTemplate Bean 중복 문제 해결

--- src/main/java/work/trade/configuration/RedisConfig.java
+++ src/main/java/work/trade/configuration/RedisConfig.java
@@ -2,6 +2,7 @@ package work.trade.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -11,6 +12,7 @@ public class RedisConfig {
 
     //RedisTemplate 설정
     @Bean
+    @Primary
     public RedisTemplate<String, String> redisTemplate(
             RedisConnectionFactory connectionFactory) {
 
