5e24e22ab72222df2b8667ad42ba27fb3e869334
wlstjd50 <answk426@naver.com>
2026-04-26 01:47:16 +0900

    User N+1 문제 최적화

--- src/main/java/work/trade/user/repository/UserRepository.java
+++ src/main/java/work/trade/user/repository/UserRepository.java
@@ -1,10 +1,24 @@
 package work.trade.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import work.trade.user.domain.User;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
+
+    //===============JOIN FETCH FUNC==================//
+    @Query("select u from User u " +
+            "join fetch u.authProvider " +
+            "where u.id = :id")
+    Optional<User> findByIdFetchJoin(@Param("id") Long id);
+
+    @Query("select u from User u " +
+            "join fetch u.authProvider " +
+            "where u.email = :email")
+    Optional<User> findByEmailFetchJoin(@Param("email") String email);
 }
