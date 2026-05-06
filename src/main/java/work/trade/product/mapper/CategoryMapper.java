186ba756ba0b21d3e6f8fcaadccfa1f2f2be7655
wlstjd50 <answk426@naver.com>
2026-05-06 15:46:12 +0900

    CategoryMapper 수정 - Category -> CategoryDto parentId,Name 추가 - CategoryDto -> Category parent객체 ignore처리

--- src/main/java/work/trade/product/mapper/CategoryMapper.java
+++ src/main/java/work/trade/product/mapper/CategoryMapper.java
@@ -1,12 +1,18 @@
 package work.trade.product.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import work.trade.product.domain.Category;
 import work.trade.product.dto.response.CategoryDto;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
+    //parent 서비스에서 넣어주기
+    @Mapping(target = "parent", ignore = true)
     Category toEntity(CategoryDto dto);
+
+    @Mapping(target = "parentId", source = "parent.id")
+    @Mapping(target = "parentName", source = "parent.name")
     CategoryDto toDto(Category category);
 }
